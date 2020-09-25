f97aa360b66f4fe53f5ef7b6b3a1829ade758480
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-09-25 23:23:58 +0300

    made 5 tasks

---
+++ homework3.2.py
@@ -0,0 +1,30 @@
+# value = int(input('Введи число:'))
+# new_value = int(value) / 2 if value < 100 else str(value)[::-1]
+# print(new_value)
+#####################################################################
+# value = int(input('Введи число:'))
+# new_value = 1 if value < 100 else 0
+# print(new_value)
+#####################################################################
+# value = int(input('Введи число:'))
+# new_value = bool(value < 100)
+# print(new_value)
+#####################################################################
+# my_str = input('Напишите слово или предложение:')
+# for i in my_str:
+#   if i .islower() and i .isalpha():
+#     print(i .upper())
+#####################################################################
+# my_str = input('Напишите слово или предложение:')
+# for i in my_str:
+#    if i .isupper() and i .isalpha():
+#      print(i .lower())
+
+
+
+
+
+
+
+
+
