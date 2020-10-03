faafbe4362e9ed0f7d0fc922e35f9047cc6f6886
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-03 09:28:47 +0300

    uncommented

--- homework3.2.py
+++ homework3.2.py
@@ -1,51 +1,47 @@
-# value = int(input('Введи число:'))
-# new_value = int(value) / 2 if value < 100 else str(value)[::-1]
-# print(new_value)
+value = int(input('Введи число:'))
+new_value = int(value) / 2 if value < 100 else str(value)[::-1]
+print(new_value)
 #####################################################################
-# value = int(input('Введи число:'))
-# new_value = 1 if value < 100 else 0
-# print(new_value)
+value = int(input('Введи число:'))
+new_value = 1 if value < 100 else 0
+print(new_value)
 #####################################################################
-# value = int(input('Введи число:'))
-# new_value = bool(value < 100)
-# print(new_value)
+value = int(input('Введи число:'))
+new_value = bool(value < 100)
+print(new_value)
 #####################################################################
-# my_str = input('Напишите слово или предложение:')
-# for i in my_str:
-#   if i .islower() and i .isalpha():
-#     print(i .upper())
+my_str = input('Напишите слово или предложение:')
+for i in my_str:
+  if i .islower() and i .isalpha():
+    print(i .upper())
 #####################################################################
-# my_str = input('Напишите слово или предложение:')
-# for i in my_str:
-#    if i .isupper() and i .isalpha():
-#      print(i .lower())
+my_str = input('Напишите слово или предложение:')
+for i in my_str:
+   if i .isupper() and i .isalpha():
+     print(i .lower())
 #####################################################################
-# my_str = input('Напишите слово:')
-# my_str = my_str * 2 if len(my_str) < 5 else my_str
-# print(my_str)
+my_str = input('Напишите слово:')
+my_str = my_str * 2 if len(my_str) < 5 else my_str
+print(my_str)
 #####################################################################
-# my_str = (input('Напишите слово:'))
-# my_str = str(my_str) + str(my_str)[::-1] if len(my_str) < 5 else my_str
-# print(my_str)
+my_str = (input('Напишите слово:'))
+my_str = str(my_str) + str(my_str)[::-1] if len(my_str) < 5 else my_str
+print(my_str)
 ######################################################################
-# my_str = (input('Напишите предложение:'))
-# for i in my_str:
-#     if i .isalpha() or i .isdigit():
-#         print(i)
+my_str = (input('Напишите предложение:'))
+for i in my_str:
+    if i .isalpha() or i .isdigit():
+        print(i)
 #####################################################################
-# my_str = (input('Напишите предложение:'))
-# for i in my_str:
-#     if not i .isalpha() and not i .isdigit():
-#         print(i)
-#####################################################################
-# my_str = (input('Напишите предложение:'))
-# for i in my_str:
-#     if not i.isalpha() and not i.isdigit() and not i .isspace():
-#         print(i)
-
-
-
-
+my_str = (input('Напишите предложение:'))
+for i in my_str:
+    if not i .isalpha() and not i .isdigit():
+        print(i)
+####################################################################
+my_str = (input('Напишите предложение:'))
+for i in my_str:
+    if not i.isalpha() and not i.isdigit() and not i .isspace():
+        print(i)
 
 
 
