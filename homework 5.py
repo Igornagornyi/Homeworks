69dc1f3d3e1c40972afae5d8d3ecb6c11dc33fd8
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-04 23:22:03 +0300

    corrected 10th task

--- homework 5.py
+++ homework 5.py
@@ -4,7 +4,7 @@ value = str(value)
 my_value = value.count('0')
 print(int(my_value))
 # ####################################################################
-# #2
+#2
 my_list = []
 value = 30500060006
 my_value = list(str(value)[::-1])
@@ -15,28 +15,28 @@ for i in my_value:
          break
 print(len(my_list))
 # ####################################################################
-# #3
+#3
 my_list_1 = [1, 4, 5, 10, 18, 99]
 my_list_2 = [34, 78, 90, 46, 75]
 my_result =[]
 my_result.extend((my_list_1)[0:-1:2])
 my_result.extend((my_list_2)[1:-1:2])
 print(my_result)
-# #####################################################################
-# #4
+######################################################################
+#4
 my_list = [9, 5, 7, 67, 88, 3]
 new_list = []
 new_list.extend((my_list)[1:len(my_list)])
 new_list.extend((my_list)[:1])
 print(new_list)
-# ######################################################################
-# #5
+#######################################################################
+#5
 my_list = [2, 5, 7, 9, 33]
 result = my_list.pop(0)
 my_list.append(result)
 print(my_list)
 # ######################################################################
-# #6
+#6
 value = "33 44 78"
 value_1 = value.split()
 sum = 0
@@ -50,27 +50,7 @@ print(sum)
 #7
 value = 'asuf7pp'
 value = list(value)
-for i in range(0, len(value), 2):
-value.insert()
-value.extend('_') if len(value) % 2 != 0 else None
-value_1 = []
-value_2 = []
-value_1.extend((value)[0:len(value):2])
-value_2.extend((value)[1:len(value):2])
-value_1.extend(value_2)
 
-new_value = ''.join(new_value)[0:len(value):2]
-print(value)
-value_1.extend((value)[0:2])
-value_1 = ''.join(value_1)
-value_2.extend((value)[2:4])
-value_2 = ''.join(value_2)
-value_3.extend((value)[4:6])
-value_3 = ''.join(value_3)
-result.append(value_1)
-result.append(value_2)
-result.append((value_3))
-print(value)
 ########################################################################
 #8
 my_str = 'My_long str'
@@ -82,7 +62,7 @@ sub_str.extend((my_str)[5:9])
 sub_str = ''.join(sub_str)
 print(str(sub_str))
 # #########################################################################
-# #9
+#9
 my_str = 'My long string'
 l_limit = "o"
 r_limit = "g"
