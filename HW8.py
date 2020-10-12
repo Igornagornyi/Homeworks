7524b1adff4e0c1e9b22750876497e81072e9f2a
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-12 14:37:56 +0300

    2nd done

--- HW8.py
+++ HW8.py
@@ -1,24 +1,38 @@
 import random
-from random import randint
-with open('domains.txt') as file:
-    lines = file.readlines()
-    for names in lines:
-       my_list = []
-       my_list_2 = []
-       my_list_3 = []
-       my_list.extend(lines)
-    for names in my_list:
-       my_list_2.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_2.append(str(names[1:len(my_list_2)]))
-number_1 = randint(1, len(my_list_2))
-number_2 = randint(1, len(my_list_2))
-number_3 = randint(1, len(my_list_2))
-try:
-    my_list_3.append(my_list_2[number_1])
-    my_list_3.append(my_list_2[number_2])
-    my_list_3.append(my_list_2[number_3])
-except IndexError:
-    pass
-
-def print_random_domains():
-    print(my_list_3)
+# from random import randint
+# with open('domains.txt') as file:
+#     lines = file.readlines()
+#     for names in lines:
+#        my_list = []
+#        my_list_2 = []
+#        my_list_3 = []
+#        my_list.extend(lines)
+#     for names in my_list:
+#        my_list_2.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_2.append(str(names[1:len(my_list_2)]))
+# number_1 = randint(1, len(my_list_2))
+# number_2 = randint(1, len(my_list_2))
+# number_3 = randint(1, len(my_list_2))
+# try:
+#     my_list_3.append(my_list_2[number_1])
+#     my_list_3.append(my_list_2[number_2])
+#     my_list_3.append(my_list_2[number_3])
+# except IndexError:
+#     pass
+#
+# def print_random_domains():
+#      print(my_list_3)
+##############################################################################################
+#2
+# with open('names.txt') as file:
+#     lines = file.readlines()
+#     for names in lines:
+#         my_list = []
+#         my_list_2 = []
+#         my_list.extend(lines)
+#     for names in my_list:
+#         my_list = names.split()
+#         my_list_2.append(my_list[1])
+#
+# def print_surnames_list():
+#     print(my_list_2)
 
