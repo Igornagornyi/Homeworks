0c1e0496537db0df186f14885c569c4b316f85fa
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-12 13:08:08 +0300

    1st done

---
+++ HW8.py
@@ -0,0 +1,24 @@
+import random
+from random import randint
+with open('domains.txt') as file:
+    lines = file.readlines()
+    for names in lines:
+       my_list = []
+       my_list_2 = []
+       my_list_3 = []
+       my_list.extend(lines)
+    for names in my_list:
+       my_list_2.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_2.append(str(names[1:len(my_list_2)]))
+number_1 = randint(1, len(my_list_2))
+number_2 = randint(1, len(my_list_2))
+number_3 = randint(1, len(my_list_2))
+try:
+    my_list_3.append(my_list_2[number_1])
+    my_list_3.append(my_list_2[number_2])
+    my_list_3.append(my_list_2[number_3])
+except IndexError:
+    pass
+
+def print_random_domains():
+    print(my_list_3)
+
