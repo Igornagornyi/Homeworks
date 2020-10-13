a0b10c09404271b53f6d957b77a3f196075f994c
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-13 18:40:07 +0300

    3rd done

--- homework 8.py
+++ homework 8.py
@@ -5,29 +5,25 @@ with open('domains.txt') as file:
     lines = file.readlines()
     my_list_dl = []
     my_list_2_dl = []
-    my_list_3_dl = []
-    my_list_dl.extend(lines)
-for names in my_list_dl:
-    my_list_2_dl.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_2_dl.append(str(names[1:len(my_list_2_dl)]))
-number_1 = randint(0, len(my_list_2_dl)-1)
-number_2 = randint(0, len(my_list_2_dl)-1)
-number_3 = randint(0, len(my_list_2_dl)-1)
-my_list_3_dl.append(my_list_2_dl[number_1])
-my_list_3_dl.append(my_list_2_dl[number_2])
-my_list_3_dl.append(my_list_2_dl[number_3])
-print(my_list_3_dl)
-
+for names in lines:
+    my_list_dl.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_dl.append(str(names[1:len(names)]))
+number_1 = randint(0, len(my_list_dl) - 1)
+number_2 = randint(0, len(my_list_dl) - 1)
+number_3 = randint(0, len(my_list_dl) - 1)
+my_list_2_dl.append(my_list_dl[number_1])
+my_list_2_dl.append(my_list_dl[number_2])
+my_list_2_dl.append(my_list_dl[number_3])
 
 def print_random_domains():
-    print(my_list_3_dl)
+    print(my_list_2_dl)
 #############################################################################################
 #2
 with open('names.txt') as file:
     lines = file.readlines()
 my_list_sur = []
 my_list_2_sur = []
-my_list_sur.extend(lines)
-for names in my_list_sur:
+for names in lines:
+    my_list_sur.append(names)
     my_list_sur = names.split()
     my_list_2_sur.append(my_list_sur[1])
 
@@ -35,7 +31,7 @@ def print_surnames_list():
     print(my_list_2_sur)
 
 def print_random_surname():
-    number = randint(0, len(my_list_2_sur))
+    number = randint(0, len(my_list_2_sur)-1)
     em_surname = my_list_2_sur[number]
     return em_surname
 ################################################################################################
@@ -44,29 +40,28 @@ with open('domains.txt') as file:
     lines = file.readlines()
     my_list = []
     my_list_2 = []
-    my_list_3 = []
-    my_list.extend(lines)
-for names in my_list:
-    my_list_2.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list_2.append(str(names[1:len(my_list_2)]))
-number_1 = randint(0, len(my_list_2)-1)
-my_list_3.append(my_list_2[number_1])
-my_list_3 = ''.join(my_list_3)
+for names in lines:
+    my_list.append(str(names[1:-1])) if str(names[-1]) == '\n' else my_list.append(str(names[1:len(names)]))
+number_1 = randint(0, len(my_list) - 1)
+my_list_2.append(my_list[number_1])
+my_list_2 = ''.join(my_list_2)
 
 def random_domain():
-    em_domain = my_list_3
+    em_domain = my_list_2
     return em_domain
 
 def get_email_number():
     email_number = randint(100, 999)
-    em_number = str(email_number)
-    return em_number
+    email_number = str(email_number)
+    return email_number
 def get_email_letters():
     email_letters = random.choices(string.ascii_lowercase, k=5)
     email_letters = ''.join(email_letters)
-    em_letters = str(email_letters)
-    return em_letters
+    return email_letters
 
 def random_email():
     email_part = print_random_surname() + '.' + get_email_number() + '@' + get_email_letters() + '.' + random_domain()
     print(email_part)
 
+
+random_email()
