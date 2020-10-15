501e748256918efff987eca098a32325f17ed1b5
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-15 22:42:07 +0300

    doing 2nd

--- homework 9.py
+++ homework 9.py
@@ -1,11 +1,12 @@
+#1
 def read_dates_and_events(path_file):
     my_list = []
     with open('authors.txt', 'r') as file:
         lines = file.readlines()
-    for data in lines:
-        r_limit = data.find('-')
-        if len(data) > 12:
-            my_list.append(data[0:r_limit+1])
+        for data in lines:
+            r_limit = data.find('-')
+            if len(data) > 10:
+                my_list.append(data[0:r_limit+1])
             if ' birthday' in data:
                 my_list.append('birthday')
             elif 'death' in data:
@@ -13,4 +14,40 @@ def read_dates_and_events(path_file):
             else:
                 pass
     return my_list
+##############################################################################################
+#2
+def read_dates(path_file):
+    my_list_date = []
+    with open('authors.txt', 'r') as file:
+        lines = file.readlines()
+        for data in lines:
+            r_limit = data.find('-')
+            if len(data) > 12 and data[r_limit-2].isdigit():
+                my_list_date.append(data[0:r_limit])
+            else:
+                pass
+        return my_list_date
+def read_names(file_path):
+    my_list_name = []
+    with open('authors.txt', 'r') as file:
+        lines = file.readlines()
+        for data in lines:
+            l_limit = data.find('-')
+            r_limit = data.find("'")
+            if len(data) > 10:
+                my_list_name.append(data[l_limit+1:r_limit+1])
+            else:
+                pass
+        return my_list_name
 
+def read_name_and_date(name, date):
+    my_dict = {"name": name, "date": date}
+    return my_dict
+
+name = read_names('authors.txt')
+date = read_dates('authors.txt')
+
+
+
+
+print(read_dates('authors.txt'))
