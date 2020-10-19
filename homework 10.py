ca2f1671bfe242c82fe22bda92dd12591f3158ec
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-19 21:52:52 +0300

    2nd

--- homework 10.py
+++ homework 10.py
@@ -3,15 +3,15 @@ import json
 import random
 from random import randint
 import string
-# def random_str_with_symbols():
-#     random_str = string.ascii_lowercase + string.ascii_uppercase + \
-#                  '\n\n\n\n\n\n\n\n\n' + string.digits + string.punctuation
-#     ran_size = randint(100, 1000)
-#     return ''.join(random.choice(random_str) for x in range(ran_size))
-# def write_file_txt():
-#     with open('str.txt', 'w') as file:
-#         for line in random_str_with_symbols():
-#             file.write(random_str_with_symbols())
+def random_str_with_symbols():
+    random_str = string.ascii_lowercase + string.ascii_uppercase + \
+                 '\n\n\n\n\n\n\n\n\n' + string.digits + string.punctuation
+    ran_size = randint(100, 1000)
+    return ''.join(random.choice(random_str) for x in range(ran_size))
+def write_file_txt():
+    with open('str.txt', 'w') as file:
+        for line in random_str_with_symbols():
+            file.write(random_str_with_symbols())
 # ####################################################################################################################
 #2
 
