dbf5cf1c9432b39379b8140690ef850c98aa7df8
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-20 15:15:30 +0300

    done

--- homework 2(2).py
+++
@@ -1,6 +0,0 @@
-value = input('Введите число:')
-value = int(value)
-if (value * 2) % 5 == 0:
-    print(value * 2)
-else:
-    print(0)
--- homework 8a.py
+++
@@ -1,3 +0,0 @@
-from HW8 import print_random_domains
-
-print_random_domains
