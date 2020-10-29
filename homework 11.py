66839d4e3169401195300cfdcf5240ad5d80258e
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-10-29 21:08:48 +0200

    corrected 29.10.20

--- homework 11.py
+++ homework 11.py
@@ -10,7 +10,6 @@ def read_json(file_path):
         data = json.load(read_file)
     return data
 
-
 my_list = read_json(file_path)
 
 #2
@@ -19,7 +18,7 @@ def list_names(tmp_str):
     reg_ex = r"[A-z]+"
     result = re.findall(reg_ex, names_str)
     value = result[-1]
-    return len(value)
+    return value
 
 def sort_names_dict(my_list):
     sort_list = sorted(my_list, key=list_names)
@@ -38,9 +37,7 @@ def death_years_dict(my_list):
     data = sorted(my_list, key=sort_years)
     return data
 
-
 #4
-
 def sort_text(tmp_str):
     text_str = tmp_str['text']
     reg_ex = r"[A-z]"
@@ -50,7 +47,6 @@ def sort_text(tmp_str):
 def text_dict(my_list):
     data = sorted(my_list, key=sort_text)
     return data
-
 ############################################################################################################################
 
 url = 'https://api.forismatic.com/api/1.0/'
@@ -59,40 +55,33 @@ def get_response(url):
     response = requests.get(url, params=my_dict)
     return response.json()
 
-
 my_url = get_response(url)
-def url_list_names(tmp_dict):
-    my_list_names = []
-    my_list_names.append(tmp_dict['quoteAuthor'])
-    return my_list_names if tmp_dict['quoteAuthor'] else None
+counter = 0
+my_list_names = []
+my_list_text = []
+my_list_link = []
+while counter <= 500:
+    res = get_response(url)
+    if res['quoteAuthor'] and res['quoteAuthor'] not in my_list_names:
+        my_list_names.append(res['quoteAuthor'])
+        my_list_text.append(res['quoteText'])
+        my_list_link.append(res['quoteLink'])
+    counter += 1
 
 def url_list_names_sort(my_url):
-    data = sorted(my_url, key=url_list_names)
+    data = sorted(my_list_names)
     return data
 
-def url_text_list(my_url):
-    my_list_text = []
-    my_list_text.append(my_url['quoteText'])
-    return my_list_text if my_url['quoteText'] else None
-
-def url_link_list(my_url):
-    my_list_link = []
-    my_list_link.append(my_url['quoteLink'])
-    return my_list_link if my_url['quoteLink'] else None
-print(url_link_list(my_url))
 file_path = 'D:/PycharmProjects/projects/new.csw'
 def write_csv(file_path):
     with open('new.csv', 'w') as csvfile:
-        try:
-            data_1 = url_list_names(my_url)
-            data_2 = url_text_list(my_url)
-            data_3 = url_link_list(my_url)
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(['quoteAuthor', 'quoteText', 'quoteLink'])
-            csvwriter.writerows([data_1, data_2, data_3])
-        except(csv.Error):
-            pass
-
+        data_1 = url_list_names_sort(my_url)[0:20]
+        data_2 = my_list_text[0:20]
+        data_3 = my_list_link[0:20]
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['quoteAuthor', 'quoteText', 'quoteLink'])
+        csvwriter.writerows([data_1, data_2, data_3])
+print(write_csv(file_path))
 
 
 
