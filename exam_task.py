91a55853cc9b3f76606b82edf9d5662b08cecc4f
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2020-11-01 18:12:29 +0200

    corrected

--- exam_task.py
+++ exam_task.py
@@ -1,6 +1,8 @@
 import random
 import numpy
-
+import json
+import os
+import string
 def create_randome_triangle():
     A = numpy.random.uniform(-100.0, 100.0, 2)
     B = numpy.random.uniform(-100.0, 100.0, 2)
@@ -42,3 +44,44 @@ def create_triangle_with_coord(coord:tuple):
         sum = sum + i
         square = abs(sum / 2)
     return f"{square:.{2}f}"
+##################################################################################
+#2
+def ran_str_six_symb():
+    ran_dig = '0123456789_'
+    ran_lett = random.choices(string.ascii_lowercase, k=2)
+    ran_str = ''.join(random.choices(ran_dig, k=4) + ran_lett)
+    return ran_str
+
+try:
+    os.mkdir('tmp_folder')
+except FileExistsError:
+    pass
+
+
+filename = ran_str_six_symb()[0:4]
+ran_int = random.randint(4, 400)
+symbol_1 = filename[0]
+symbol_2 = filename[1]
+symbol_3 = filename[2]
+symbol_4 = filename[3]
+xmin_1 = 0 if symbol_1.isdigit() else None
+xmax_1 = ran_int // 4 if symbol_1.isdigit() else None
+xmin_2 = ran_int // 4 if symbol_2.isdigit() else None
+xmax_2 = ran_int // 2 if symbol_2.isdigit() else None
+xmin_3 = ran_int // 2 if symbol_3.isdigit() else None
+xmax_3 = ran_int - (ran_int // 4) if symbol_3.isdigit() else None
+xmin_4 = ran_int - (ran_int // 4) if symbol_4.isdigit() else None
+xmax_4 = ran_int if symbol_4.isdigit() else None
+my_data = {"filename": filename, "Width": ran_int, "objects":
+    {"object_1": {"class": symbol_1, "xmin": xmin_1, "xmax": xmax_1},
+     "object_2": {"class": symbol_2, "xmin": xmin_2, "xmax": xmax_2},
+     "object_3": {"class": symbol_3, "xmin": xmin_3, "xmax": xmax_3},
+     "object_4": {"class": symbol_4, "xmin": xmin_4, "xmax": xmax_4}}}
+
+
+
+
+
+filename = 'D:/PycharmProjects/projects' + '/tmp_folder' + '/' + ran_str_six_symb() + '.json'
+with open(filename, 'w') as json_file:
+    json.dump(my_data, json_file)
